54cbc394da03cb15893e9f3ad51e3770f05d7273
Maksim101996 <akakii.botov@mail.com>
2023-07-05 16:46:05 +0500

    start working

---
+++ src/components/main/main.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import s from './main.module.scss'
+
+
+
+
+
+const Main = () => {
+	return (
+		<div className={s.main}>hello man</div>
+	)
+}
+
+
+
+
+export default Main
